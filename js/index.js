28f721ad57061a9eeb17d242bf90404b536a0467
Mickano RAKOTOMALALA <rakotomalalamickano@gmail.com>
2024-05-01 18:00:00 +0300

    email modified

--- js/index.js
+++ js/index.js
@@ -5,6 +5,17 @@ function sendMail(){
         objet : document.getElementById("objet").value,
         message: document.getElementById("message").value,
     };
+const serviceID = "service_jtubjse"
+const templateID = "template_krsqgyj"
 
-emailjs.send("service_jitu0em" , "template_9ix6aad", params).then("alert(Email Sent!!!")
+emailjs.send("serviceID" , "templateID", params)
+.then((res) => {
+    document.getElementById("name").value = " ";
+    document.getElementById("email").value = " ";
+    document.getElementById("objet").value = " ";
+    document.getElementById("message").value = " ";
+    console.log(res);
+    alert("your email sent succefully");
+})
+.catch((err) => console.log(err));
 }
